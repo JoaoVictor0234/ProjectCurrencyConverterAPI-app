b4980233cbbbdb5f023de66742c3b28f54862b1e
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-20 09:34:25 -0300

    Cria a classe que configura e fornece uma instância retrofit

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -1,5 +1,5 @@
 // Moldelo de dados (DTO - Data Transfer Object) que representa a resposta json da API
-
+// DTO - Transferencia de dados da API para o projeto
 package com.example.projectcurrencyconverterapi;
 
 public class ExchangeRatesResponse {
