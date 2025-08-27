fdc3add46f7f3188decf4e18160291ce252011b3
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-27 09:38:49 -0300

    Cria um método para acessar as taxas de conversão e retorna ao mapa contendo as taxas armazenadas

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -1,7 +1,7 @@
 // Moldelo de dados (DTO - Data Transfer Object) que representa a resposta json da API
 // DTO - Transferencia de dados da API para o projeto
 package com.example.projectcurrencyconverterapi;
-// Importa a classe Map da biblioteca Java, usada ppara armazenazar pares de chave-valor
+// Importa a classe Map da biblioteca Java, usada para armazenazar pares de chave-valor
 
 
 import java.util.HashMap;
@@ -9,6 +9,11 @@ import java.util.Map;
 public class ExchangeRatesResponse {
 //Declara um mapa que armazena taxas de conversão em moedas.
     // A chave (String) representa o cóodigo da moeda (ex: "USD". "BRL"), e o valor (Double) representa a taxa de conversão para essa moeda.
+// chave-valor - "Usd", 1.
 
 
+    private Map<String, Double> conversion_rates = new HashMap<>();
+
+    //Métdo publico para acessar as taxas de conversão
+    //Retorna o mapa contendo as taxas de conversão armazenadas
 }
