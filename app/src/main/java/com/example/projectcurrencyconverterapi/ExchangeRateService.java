69be6620397b00b7543034df43b9522e9e63c78b
uept42-user <uept42-user@aluno.pr.senac.br>
2025-08-27 09:15:48 -0300

    Adiciona um Get que puxa o valor da API, além de criar dois Path e Substituir apiKey e baseCurrency

--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -6,6 +6,14 @@ import retrofit2.http.Path;
 public interface ExchangeRateService {
 
     /**
-     *Método para obter taxas de câmbio recentes
-     * **/
-}
+     * Método para obter taxas de câmbio recentes
+     **/
+    $GET("/v6/{apiKey}/latest/{baseCurrency}");
+    Call<ExchangeRatesResponse> getExchangeRates (
+        // Substitui {apiKey} na Url com o valor fornecido
+        @Path("apiKey") String apiKey,
+        //Substitui {baseCurrency} na Url com o valor fornecido
+        @Path("baseCurrency") String baseCurrency
+);
+    }
+
